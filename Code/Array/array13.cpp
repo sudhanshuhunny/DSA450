7bc8e35e6d1f24844f285ed57fabc8e35e99ee83
sudhanshuhunny <sudhanshusengh@gmail.com>
2021-01-31 19:04:10 +0530

    Day 8

---
+++ Code/Array/array13.cpp
@@ -0,0 +1,46 @@
+/*
+Kadane's Algorithm 
+Medium Accuracy: 51.0% Submissions: 43860 Points: 4
+Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
+
+ 
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {1,2,3,-2,5}
+Output:
+9
+Explanation:
+Max subarray sum is 9
+of elements (1, 2, 3, -2, 5) which 
+is a contiguous subarray.
+Example 2:
+
+Input:
+N = 4
+arr[] = {-1,-2,-3,-4}
+Output:
+-1
+Explanation:
+Max subarray sum is -1 
+of element (-1)
+ 
+
+Your Task:
+You don't need to read input or print anything. The task is to complete the function maxSubarraySum() which takes arr and N as input parameters and returns the sum of subarray with maximum sum.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 ≤ N ≤ 106
+-107 ≤ A[i] <= 107
+
+
+*/
