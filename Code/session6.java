3ab4dac161b18ed3b6caa1a26534906f8013d833
sudhanshuhunny <sudhanshusengh@gmail.com>
2021-02-12 21:36:13 +0530

    Recursion Continue

---
+++ Code/session6.java
@@ -0,0 +1,72 @@
+package Code;
+
+import java.util.Scanner;
+
+
+
+public class session6 {
+    public static void main(String[] args) {
+    //     // Recursion (function calling Itself)
+       Scanner s=new Scanner(System.in);
+    //     int n=5; //s.nextInt();
+    //     print(n);
+    //    // printInc(n);
+      
+    //    //fact(n);
+    //    System.out.println(fact(n));
+
+
+    //    int [] nums={2,5,6,7,8,9,6,4,6,2,8};
+    //    System.out.println(find(nums,5,0));
+    // }
+
+    // public static boolean find(int[] nums, int elt, int ind) {
+    //     if(ind==nums.length){
+    //         return false;
+    //     }
+    //     if(nums[ind]==elt){
+    //         return true;
+    //     }
+    //     // if(nums[ind]==nums.length){
+    //     //     return false;
+    //     // } it should not be placed here coz it will make a ind that out of bound to be search
+
+    //     return find(nums,elt,ind+1);
+
+    // }
+
+    // public static int fact(int n) {
+
+    //     if(n==0)
+    //     {
+    //         return 1;
+    //     }
+    //     else 
+    //     {return n*fact(n-1);}
+    // }
+
+    // public static void print(int n) {
+    //     if(n==0)
+    //     {
+    //         return;
+    //     } // If not there then it will go till -17705 and boom error
+    //     System.out.println(n);
+    //     print(n-1);
+    // }
+
+    // public static void printInc(int n) {
+    //     if(n==0)
+    //     {
+    //         return;
+    //     }
+        
+    //     printInc(n-1); // Pushing in the Stack
+    //     System.out.println(n); // Poping from the stack
+        
+
+
+
+}
+    
+}
+
