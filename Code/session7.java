3c4392c6d324218407ffdb14b1aeb395d88bd8cc
sudhanshuhunny <sudhanshusengh@gmail.com>
2021-02-18 20:41:26 +0530

    rev

--- Code/session7.java
+++ Code/session7.java
@@ -4,16 +4,13 @@ import java.util.Scanner;
 
 public class session7 {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        String n =s.nextLine();
-        System.out.println(n);
 
 
 
         // To reslove the problem of the recursion tree in Last Problem
         // optimising the last Problem in session 6
-
-
+        
+        
 
     }   
 }
